c569b347059ec9586f8a14dba5c237fb39a93117
LoRd_MuldeR <mulder2@gmx.de>
2018-05-27 22:04:38 +0200

    Detect the actual max. number of CPU cores at runtime.

---
+++ MPatchCLI/src/sysinfo.c
@@ -0,0 +1,46 @@
+/* ---------------------------------------------------------------------------------------------- */
+/* MPatch - simple patch and compression utility                                                  */
+/* Copyright(c) 2018 LoRd_MuldeR <mulder2@gmx.de>                                                 */
+/*                                                                                                */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy of this software  */
+/* and associated documentation files (the "Software"), to deal in the Software without           */
+/* restriction, including without limitation the rights to use, copy, modify, merge, publish,     */
+/* distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the  */
+/* Software is furnished to do so, subject to the following conditions:                           */
+/*                                                                                                */
+/* The above copyright notice and this permission notice shall be included in all copies or       */
+/* substantial portions of the Software.                                                          */
+/*                                                                                                */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING  */
+/* BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND     */
+/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,   */
+/* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        */
+/* ---------------------------------------------------------------------------------------------- */
+
+#include "sysinfo.h"
+
+#define WIN32_LEAN_AND_MEAN 1
+#include <Windows.h>
+
+uint_fast32_t get_processor_count(void)
+{
+	DWORD_PTR maskProcess, maskSystem;
+	if (GetProcessAffinityMask(GetCurrentProcess(), &maskProcess, &maskSystem))
+	{
+		uint_fast32_t count = 0U;
+		while (maskProcess)
+		{
+			if (maskProcess & 1U)
+			{
+				count++;
+			}
+			maskProcess >>= 1U;
+		}
+		if (count)
+		{
+			return count;
+		}
+	}
+	return 1U;
+}
