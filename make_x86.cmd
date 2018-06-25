98c061beb7c00cff5db7f690756f7315803f1d86
LoRd_MuldeR <mulder2@gmx.de>
2018-06-25 22:07:24 +0200

    Added PGO build scripts.

---
+++ make_x86.cmd
@@ -0,0 +1,43 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+cd /d "%~dp0"
+
+
+echo.
+echo ===================================================
+echo # INSTRUMENTING
+echo ===================================================
+echo.
+
+msbuild.exe /t:Rebuild "%~dp0\MPatch.sln" /p:Configuration=Release /p:Platform=x86 /p:WholeProgramOptimization=PGInstrument
+if %ERRORLEVEL% NEQ 0 goto:BuildError
+
+
+echo.
+echo ===================================================
+echo # PROFILING
+echo ===================================================
+
+"%~dp0\bin\Win32\Release\MPatchCLI.exe" -c "%~dp0\etc\opusenc.new.exe" "%~dp0\etc\opusenc.old.exe" "%TMP%\~%RANDOM%%RANDOM%.patch"
+if %ERRORLEVEL% NEQ 0 goto:BuildError
+
+
+echo.
+echo ===================================================
+echo # BUILDING (PGO)
+echo ===================================================
+echo.
+
+msbuild.exe /t:LibLinkOnly "%~dp0\MPatch.sln" /p:Configuration=Release /p:Platform=x86 /p:WholeProgramOptimization=PGOptimize /p:LinkTimeCodeGeneration=PGOptimization /p:ProfileGuidedDatabase=""%~dp0\bin\Win32\Release\MPatchCLI.pgd"
+if %ERRORLEVEL% NEQ 0 goto:BuildError
+
+echo.
+echo Completed.
+pause
+goto:eof
+
+
+:BuildError
+echo.
+echo Whoops, something went wrong !!!
+pause
